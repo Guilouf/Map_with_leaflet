c0b672c16b37687ba53da1566ec9a378172b2cc8
Guilouf <guigore@wanadoo.fr>
2016-12-26 21:48:24 +0100

    added functions to select from criterium and refresh geojson markers

--- map_js.js
+++ map_js.js
@@ -14,7 +14,9 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		noWrap: true //pr eviter la map infinie, mm si on peut foutre des marquers en dehors..
 		}).addTo(map);
 
-//ajoute des markers, ac un message. ca instancie en plus
+/*
+add markers from coords and binds a pop up message
+*/
 function aj_marker(coord, message) {
     L.marker(coord).addTo(map)
             .bindPopup(message)
@@ -30,42 +32,60 @@ aj_marker([47.4, 1.5], 'MERDE');
 window.L_PREFER_CANVAS=true;  //améliore un peu les perf, mais vite fait..
 
 /*
-sort the geojson, todo have différent selectors..
+sort the geojson, from the filter tab
 */
-function sort_geojson (geo_var){
+function sort_geojson (geo_var, filter){
     var sorted_geoj = {
         "type": "FeatureCollection",
         "features": []};
-//        alert(geo_var["features"][0]["type"]);
     var features = geo_var["features"];
     for (var index in geo_var["features"]) { // ya pas de collection en js, la merde..
-//        alert(features[index]["type"]);
-        if (features[index]["properties"]["activity"] == true) {
-            alert(features[index]["properties"]["activity"]);
+        var bool_push = false
+        if (features[index]["properties"]["activity"] == filter[0] || filter[0] == null) { //select act or inactivity
+            bool_push = true;
+        } else {
+            bool_push = false;
+        }
+        // basic search engine..
+//        if (features[index]["properties"]["name"].includes(filter[1]) || filter[1] == null) {
+//            bool_push = true;
+//        } else {
+//            bool_push = false;
+//        }
+        if (bool_push) { // allow multiple criterium selection
             sorted_geoj["features"].push(features[index]); // bah oui, append c'est trop simple..
         }
     }
     return sorted_geoj
 };
 
-geojson_ext = sort_geojson(geojson_ext); //rewrite with exclusions
-
+//geojson_ext = sort_geojson(geojson_ext, [null, ""]);
 /*
-Add the markers from geojson, link their properties with the popup
+Add the markers from geojson, call sort function, link their properties with the popup
 */
-L.geoJson(geojson_ext, { //todo petits soucis de perf.. il faut le plug leaflet.markercluster,
-    onEachFeature: function (feature, layer) { // pose pas de pb de perf
-    // todo changer la couleur du marker en fonction des propriétés
-//        layer.bindPopup(feature.properties.description);
-        var popupcontent = [];
-        for (var prop in feature.properties) { // permet d'iterer ttes les properties du geojson et de les afficher
-            popupcontent.push(prop + ": " + feature.properties[prop]);
+function map_geojson(geojson_ext) {
+    // maintenant faut rafraichir..
+    var select = document.getElementById('selec_prop');
+    var bool_friche = select.getElementsByClassName('selector')[0].value == "true" ? false : true;
+    //alert(div_selector.getElementsByClassName('selector')[0].value)
+    geojson_ext = sort_geojson(geojson_ext, [bool_friche,
+        null]);
+
+    var layer_geoj = L.geoJson(geojson_ext, { //todo petits soucis de perf.. il faut le plug leaflet.markercluster,
+        onEachFeature: function (feature, layer) { // pose pas de pb de perf
+        // todo changer la couleur du marker en fonction des propriétés
+    //        layer.bindPopup(feature.properties.description);
+            var popupcontent = [];
+            for (var prop in feature.properties) { // permet d'iterer ttes les properties du geojson et de les afficher
+                popupcontent.push(prop + ": " + feature.properties[prop]);
+            }
+            layer.bindPopup(popupcontent.join("<br />"));
         }
-        layer.bindPopup(popupcontent.join("<br />"));
-    }
-}).addTo(map);
-
-
+    }).addTo(map);
+    return layer_geoj //the layer object, returned to deleted on refresh
+};
+var layer = map_geojson(geojson_ext); // called from html
+//map.removeLayer(layer); // called in the html
 
 var coor_click = [0,0];
 //Affichage des coordonnées au clic
